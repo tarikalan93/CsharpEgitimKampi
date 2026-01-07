e317febdc02d72065a746aefb9875448ca7ccaf2
TARIK ALAN <TARIK.ALAN@lcwaikiki.com>
2026-01-07 14:54:30 +0300

    yeni örnekeler

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Lütfen yazıyı kaç kez yazdırmak istediğinizi giriniz: ");
+            //int sayi = int.Parse(Console.ReadLine());
+            //for (int i=1; i<=sayi; i++)
+            //{
+            //    Console.WriteLine(i + ". Merhaba Dünya!");
+            //}
+
+            //int totalValue = 0;
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+            //Console.WriteLine("toplam değer: " + totalValue);
+
+            //int totalValue = 0;
+            //for (int i  = 0; i  < 50; i ++)
+            //{
+            //    if (i % 7 ==0 )
+            //    {
+            //        totalValue += i;
+
+            //    }
+            //    Console.WriteLine(totalValue);
+            //}
+
+            //for (int i = 1; i <= 10; i++) //1. soru
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 1; i <= 20; i++) //2. soru
+            //{
+            //    if (i % 2 == 0)
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 10; i >= 1; i --)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 1; i <= 50; i++) //4. soru
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int kare= 0; //5. soru
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    kare = i * i;
+            //    Console.WriteLine(i + "->" + kare);
+            //}
+
+            //int toplam = 0;
+            //for (int i = 1; i <= 100; i++) //6. soru
+            //{
+            //    toplam += i;
+
+            //}
+            //Console.WriteLine(toplam);
+
+            //int sayi = 0;
+            //Console.WriteLine("Lütfen bir sayı giriniz: "); //7. soru
+            //sayi = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= sayi; i++)
+            //{
+
+            //    Console.WriteLine(i);
+
+            //}
+
+            //int sayi, toplam = 0;
+            //Console.WriteLine("Lütfen bir sayı giriniz: "); //8. soru   
+            //sayi = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= sayi; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        toplam = toplam + i;
+
+            //    }
+
+            //}
+            //Console.WriteLine(toplam);
+
+            //for (int i = 0; i <= 100; i++) //9. soru
+            //{
+            //    if (i%5==0 & i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    for (int j = 1; j < 11; j++)
+            //    {
+            //        Console.WriteLine(i + " x " + j + " = " + (i * j));
+            //    }
+
+            //}
+
+            //int sayi=0,sum = 0; //10. soru
+
+            //while (sayi <= 10)
+            //{
+                
+            //        sum += sayi;
+            //        sayi++; 
+
+            //}
+            //Console.WriteLine("Toplam: " + sum);
+        }
+    }
+}
---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region alt alta 10 tane yıldız
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region her satırda 10 yıldız alt alta 10 satır
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region dik üçgen yıldız
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //   Console.WriteLine();
+            //}
+
+            #endregion
+
+        }
+    }
+}
